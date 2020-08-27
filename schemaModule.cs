53c65b150e4a8010a140dcf489637c75a626df16
Jeffrey Patton <jeffrey@patton-tech.com>
2020-08-27 10:26:27 -0500

    Initial commit of module

---
+++ schemaModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace schema.module
+{
+ [Cmdlet(VerbsCommon.Get, "Schema")]
+ public class GetSchema : PSCmdlet
+ {
+  [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ParameterSetName = "File")]
+  public string Path { get; set; }
+  [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ParameterSetName = "Url")]
+  public string Uri { get; set; }
+  protected override void ProcessRecord()
+  {
+   switch (ParameterSetName)
+   {
+    case "File":
+     break;
+    case "Uri":
+     break;
+   }
+  }
+ }
+}
